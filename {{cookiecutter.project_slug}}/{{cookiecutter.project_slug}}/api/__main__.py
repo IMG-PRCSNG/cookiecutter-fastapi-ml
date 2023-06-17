d9656734e00810fdddd759d29a6711e97bedfa40
IMG-PRCSNG <samthedevil.sp@gmail.com>
2023-06-17 14:49:43 +0100

    Fix wrong key from config in api

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/api/__main__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/api/__main__.py
@@ -5,7 +5,7 @@ _config = Config()
 
 uvicorn.run(
     "{{ cookiecutter.project_slug }}.api:serve",
-    host=_config.hostname,
+    host=_config.host,
     port=_config.port,
     reload=True,
     factory=True,
